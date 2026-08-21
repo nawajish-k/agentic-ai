c81f5bc4961a593f76eed6a7a42ea690faac981f
Nawajish Khan <hello.nawajishkhan@gmail.com>
2026-08-21 23:54:01 +0530

    feat: build initial chat interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,35 @@
 import React from "react";
+import { RiChatNewLine, RiArrowRightFill } from "@remixicon/react";
 
 const App = () => {
   return (
     <div className="container">
-      <div className="card">
+      {/* navbar */}
+      <div className="navbar">
         <div className="icon">✦</div>
-
         <h1>Agentic AI</h1>
 
-        <p>
-          This AI agent currently runs only in the
-          <span> terminal.</span>
-        </p>
+        <button className="new-chat">
+          <RiChatNewLine size={20} className="new-chat-icon" />
+          <h3>New chat</h3>
+        </button>
+      </div>
 
-        <div className="terminal">
-          <div>
-            <span>$</span> cd backend
+      {/* chat */}
+      <div className="chat-container">
+        <div className="chat-content">
+          <div className="welcome">
+            <div className="welcome-icon">✦</div>
+            <h2>How can I help you?</h2>
+            <p>Ask anything and start a conversation with Agentic AI.</p>
           </div>
 
-          <div>
-            <span>$</span> node ./index.js
+          <div className="chat-input">
+            <input type="text" placeholder="Type here..." />
+            <button className="send-button">
+              <RiArrowRightFill/>
+            </button>
           </div>
-        </div>
-
-        <div className="info">
-          <p>
-            The web interface is currently in development.
-          </p>
-
-          <p>
-            This page is only a project information screen for now.
-          </p>
         </div>
       </div>
     </div>
